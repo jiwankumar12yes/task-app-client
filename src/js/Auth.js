cfb8723fa566e1fd4d4ab23b3f947e98ae694f31
Jiwan kumar <jiwankumar12yes@gmail.com>
2025-11-01 22:06:54 +0530

    Add .gitignore to exclude node_modules and logs

---
+++ src/js/Auth.js
@@ -0,0 +1,213 @@
+const loginBtn = document.getElementById("login");
+const signupBtn = document.getElementById("signup");
+const loginForm = document.getElementById("login-form");
+const signupForm = document.getElementById("signup-form");
+const loginEmail = document.getElementById("email-login");
+const loginPassword = document.getElementById("login-password");
+const signupName = document.getElementById("name");
+const signupPassword = document.getElementById("password");
+const signupEmail = document.getElementById("email");
+const loginToSignUpTab = document.getElementById("logToSignup");
+const submitLogin = document.getElementById("submit-loginBtn");
+const submitSignup = document.getElementById("sign-upBtn");
+const toggleButton = document.getElementById("navToggleBtn");
+const navMenu = document.getElementById("navbarMenu");
+
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+const API_BASE_URL = "https://task-app-backend-cruj.onrender.com/api/v1/auth";
+const api = axios.create({ baseURL: API_BASE_URL });
+
+toggleButton.addEventListener("click", () => {
+  if (navMenu.classList.contains("d-none")) {
+    navMenu.classList.remove("d-none");
+    navMenu.classList.add("d-block");
+  } else {
+    navMenu.classList.remove("d-block");
+    navMenu.classList.add("d-none");
+  }
+});
+
+function openLoginTab() {
+  signupForm.reset();
+  const inputs = signupForm.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.classList.remove("is-valid", "is-invalid");
+  });
+  signupForm.classList.add("d-none");
+  signupBtn.classList.remove("active-tab");
+  loginForm.classList.remove("d-none");
+  loginBtn.classList.add("active-tab");
+}
+
+function openSignupTab() {
+  loginForm.reset();
+  const inputs = loginForm.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.classList.remove("is-valid", "is-invalid");
+  });
+  loginForm.classList.add("d-none");
+  loginBtn.classList.remove("active-tab");
+  signupBtn.classList.add("active-tab");
+  signupForm.classList.remove("d-none");
+  signupForm.classList.add("d-block");
+}
+
+function onEmailChange(event) {
+  const inputElement = event.target;
+  const emailValue = inputElement.value;
+  const isValidEmail = emailRegex.test(emailValue);
+  if (isValidEmail) {
+    inputElement.classList.add("is-valid");
+    inputElement.classList.remove("is-invalid");
+  } else {
+    inputElement.classList.remove("is-valid");
+    inputElement.classList.add("is-invalid");
+  }
+}
+
+function onPasswordChange(event) {
+  // debugger;
+  const inputElement = event.target;
+  const passwordValue = inputElement.value;
+  if (passwordValue.length >= 4) {
+    inputElement.classList.add("is-valid");
+    inputElement.classList.remove("is-invalid");
+  } else {
+    inputElement.classList.remove("is-valid");
+    inputElement.classList.add("is-invalid");
+  }
+}
+
+function onNameChange() {
+  const nameValue = signupName.value;
+  if (nameValue.length >= 3) {
+    signupName.classList.add("is-valid");
+    signupName.classList.remove("is-invalid");
+  } else {
+    signupName.classList.remove("is-valid");
+    signupName.classList.add("is-invalid");
+  }
+}
+
+async function sendLogin() {
+  const email = loginEmail.value;
+  const password = loginPassword.value;
+  try {
+    const response = await api.post("/login", {
+      email: email,
+      password: password,
+    });
+
+    const result = response.data;
+
+    Toastify({
+      text: result.message,
+      duration: 1500,
+      style: {
+        background: "green",
+      },
+    }).showToast();
+    localStorage.setItem("userName", result.data.user.name);
+    localStorage.setItem("accessToken", result.data.accessToken);
+    location.replace("/dashboard.html");
+  } catch (error) {
+    Toastify({
+      text: error.response ? error.response.data.message : error.message,
+      duration: 1500,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+  } finally {
+    loginEmail.value = "";
+    loginPassword.value = "";
+    loginEmail.classList.remove("is-valid", "is-invalid");
+    loginPassword.classList.remove("is-valid", "is-invalid");
+  }
+}
+
+async function sendSignup() {
+  const name = signupName.value;
+  const email = signupEmail.value;
+  const password = signupPassword.value;
+
+  try {
+    const response = await api.post("/register", { name, email, password });
+    const result = response.data;
+    console.log(result);
+    Toastify({
+      text: result.message,
+      duration: 1500,
+      style: {
+        background: "green",
+      },
+    }).showToast();
+  } catch (error) {
+    Toastify({
+      text: error.response ? error.response.data.message : error.message,
+      duration: 1500,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+  } finally {
+    signupName.value = "";
+    signupEmail.value = "";
+    signupPassword.value = "";
+    signupEmail.classList.remove("is-valid", "is-invalid");
+    signupPassword.classList.remove("is-valid", "is-invalid");
+    signupName.classList.remove("is-valid", "is-invalid");
+  }
+}
+
+loginBtn.addEventListener("click", openLoginTab);
+signupBtn.addEventListener("click", openSignupTab);
+
+loginEmail.addEventListener("input", onEmailChange);
+loginPassword.addEventListener("input", onPasswordChange);
+signupEmail.addEventListener("input", onEmailChange);
+signupPassword.addEventListener("input", onPasswordChange);
+signupName.addEventListener("input", onNameChange);
+loginToSignUpTab.addEventListener("click", openSignupTab);
+
+submitLogin.addEventListener("click", () => {
+  if (
+    loginEmail.value !== null &&
+    loginEmail.value.length > 0 &&
+    loginPassword.value.length >= 4
+  ) {
+    sendLogin();
+  } else {
+    Toastify({
+      text: "Kindly check email and password",
+      duration: 1500,
+    }).showToast();
+  }
+});
+
+submitSignup.addEventListener("click", (event) => {
+  event.preventDefault();
+  let message = "";
+  if (signupName.value.length < 3) {
+    message = "Name should be at least 3 character";
+  } else if (
+    signupEmail.value.length !== null &&
+    signupEmail.value.length < 4
+  ) {
+    message = "check your email";
+  } else if (signupPassword.value.length < 4) {
+    message = "password should be at least 4 character";
+  }
+  if (message !== "") {
+    Toastify({
+      text: message,
+      duration: 1500,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+  } else {
+    sendSignup();
+  }
+});
---
+++ src/js/Dashboard.js
@@ -0,0 +1,533 @@
+const navMenu = document.getElementById("navbarMenu");
+const sideBarButton = document.getElementById("sidebar-button");
+const sidebarIcon = document.getElementById("sidebar-icon");
+const sidebarContainer = document.getElementById("sidebar-container");
+const userNameBanner = document.getElementById("userName");
+const addTaskBtn = document.getElementById("Add-Task-Btn");
+const taskCard = document.getElementById("task-card-data");
+const taskContainer = document.getElementById("task-container");
+const sidebarTxt = document.querySelectorAll(".sidebar-text");
+const logoutBtn = document.getElementById("logout-btn");
+const addTaskModalBtn = document.getElementById("task-modal");
+const closeTaskModalBtn = document.getElementById("close-task");
+const titleInput = document.getElementById("title");
+const descriptionInput = document.getElementById("description");
+const dueDateInput = document.getElementById("due-date");
+const submitTaskBtn = document.getElementById("sbmt-task-btn");
+const taskForm = document.getElementById("addTaskForm");
+const taskButton = document.getElementById("task-button");
+const timer = document.getElementById("timer");
+const searchTask = document.getElementById("search-task");
+
+const API_BASE_URL = "https://task-app-backend-cruj.onrender.com/api/v1/task";
+const api = axios.create({ baseURL: API_BASE_URL });
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+document.addEventListener("DOMContentLoaded", async () => {
+  getAllTasks();
+  UserName();
+  runTimer;
+});
+
+let min = 15,
+  sec = 0;
+const runTimer = setInterval(() => {
+  // const decodedPayload = decodeJwt(localStorage.getItem("accessToken"));
+  // console.log(decodedPayload);
+  if (userNameBanner.innerText === "Unknown") {
+    location.replace("index.html");
+    clearInterval(runTimer);
+    console.log("if part");
+  } else {
+    timer.innerText = `${min} m:${sec} s`;
+    if (min === 0 && sec === 0) {
+      timer.innerText = "Timeout";
+      clearInterval(runTimer);
+      localStorage.clear();
+      location.replace("index.html");
+      console.log("else part");
+    }
+    if (sec === 0) {
+      min--;
+      sec = 60;
+    } else {
+      sec--;
+    }
+  }
+}, 1000);
+
+let allTasks = [];
+let filteredTask = [];
+
+// from validation
+function validTitle(e) {
+  const inputElement = e.target;
+  const titleValue = inputElement.value;
+  if (titleValue.length <= 2) {
+    titleInput.classList.remove("is-valid");
+    titleInput.classList.add("is-invalid");
+  } else {
+    titleInput.classList.add("is-valid");
+    titleInput.classList.remove("is-invalid");
+  }
+}
+function validDescription(e) {
+  const inputElement = e.target;
+  const descriptionValue = inputElement.value;
+  if (descriptionValue.length <= 3) {
+    inputElement.classList.remove("is-valid");
+    inputElement.classList.add("is-invalid");
+  } else {
+    inputElement.classList.add("is-valid");
+    inputElement.classList.remove("is-invalid");
+  }
+}
+
+function validDueDate(e) {
+  const inputElement = e.target;
+  const selectedDateString = inputElement.value;
+  inputElement.isCustomInvalid = false;
+  const selectedDate = new Date(selectedDateString);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  selectedDate.setHours(0, 0, 0, 0);
+
+  if (selectedDate < today) {
+    inputElement.isCustomInvalid = true;
+    inputElement.classList.add("is-invalid");
+    inputElement.nextElementSibling.textContent =
+      "Due date cannot be in the past.";
+    inputElement.classList.remove("is-valid");
+  } else {
+    inputElement.classList.remove("is-invalid");
+    inputElement.classList.add("is-valid");
+  }
+}
+
+titleInput.addEventListener("input", (e) => {
+  validTitle(e);
+});
+descriptionInput.addEventListener("input", (e) => {
+  validDescription(e);
+});
+
+dueDateInput.addEventListener("change", (e) => {
+  validDueDate(e);
+});
+
+// end of validation
+
+// opening and closing modal for adding task
+function openAddTaskModal() {
+  taskForm.reset();
+  taskForm.classList.remove("was-validated");
+  taskForm
+    .querySelectorAll(".form-control, .form-select, .form-check-input")
+    .forEach((control) => {
+      control.classList.remove("is-valid", "is-invalid");
+    });
+  addTaskModalBtn.classList.remove("d-none");
+  addTaskModalBtn.classList.add("d-block");
+}
+
+function closeAddTaskModal() {
+  taskForm.reset();
+  taskForm.classList.remove("was-validated");
+  taskForm
+    .querySelectorAll(".form-control, .form-select, .form-check-input")
+    .forEach((control) => {
+      control.classList.remove("is-valid", "is-invalid");
+    });
+  addTaskModalBtn.classList.add("d-none");
+  addTaskModalBtn.classList.remove("d-block");
+}
+
+addTaskBtn.addEventListener("click", openAddTaskModal);
+closeTaskModalBtn.addEventListener("click", closeAddTaskModal);
+
+//end  opening and closing modal for adding task
+
+sideBarButton.addEventListener("click", () => {
+  const isExpanded = sidebarContainer.classList.contains("sidebar-expanded");
+
+  if (!isExpanded) {
+    sidebarIcon.src = "./src/images/sidebarClose.png";
+
+    Array.from(sidebarTxt).forEach((v) => {
+      v.classList.remove("d-none");
+      v.classList.add("d-inline-block");
+    });
+    sidebarContainer.classList.remove("sidebar-default");
+    sidebarContainer.classList.add("sidebar-expanded");
+  } else {
+    sidebarIcon.src = "./src/images/sidebarOpen.png";
+    Array.from(sidebarTxt).forEach((v) => {
+      v.classList.add("d-none");
+      v.classList.remove("d-inline-block");
+    });
+    sidebarContainer.classList.remove("sidebar-expanded");
+    sidebarContainer.classList.add("sidebar-default");
+  }
+});
+
+function UserName() {
+  const userFullName = localStorage.getItem("userName");
+  if (userFullName) {
+    const userNameArray = userFullName.split(" ");
+    const user = userNameArray[0];
+    userNameBanner.innerText = user;
+  } else {
+    userNameBanner.innerText = "Unknown";
+  }
+}
+
+searchTask.addEventListener("input", (e) => {
+  const searched = e.target.value.toLowerCase().trim();
+
+  const searchedTasks = allTasks.filter(
+    (task) =>
+      task.title.toLowerCase().includes(searched) ||
+      task.description.toLowerCase().includes(searched)
+  );
+  renderTasks(searchedTasks);
+});
+
+document.querySelector(".sidebar-filter").addEventListener("click", (e) => {
+  const clickedButton = e.target.closest("button");
+  if (!clickedButton) return;
+  const currentActive = document.querySelector(".sidebar-filter .active");
+  if (currentActive) {
+    currentActive.classList.remove("active", "text-muted", "text-light");
+    const prevText = currentActive.querySelector(".sidebar-text");
+    if (prevText) {
+      prevText.classList.remove("text-light");
+      prevText.classList.add("text-muted", "text-danger");
+    }
+  }
+  clickedButton.classList.add("active");
+  const newText = clickedButton.querySelector(".sidebar-text");
+  if (newText) {
+    newText.classList.remove("text-muted", "text-danger");
+    newText.classList.add("text-light");
+  }
+
+  const filterTask = e.target.textContent.trim();
+  switch (filterTask) {
+    case "In-Process":
+      filteredTask = allTasks.filter((task) => task.status === "In_Process");
+      break;
+
+    case "Completed":
+      filteredTask = allTasks.filter((task) => task.status === "Completed");
+      break;
+
+    // case "Calender":
+    //   break;
+
+    default:
+      filteredTask = allTasks;
+      break;
+  }
+  renderTasks(filteredTask);
+});
+
+function displayTask(task) {
+  const statusText = task.status.replace("_", " ");
+  const statusColor =
+    task.status === "Completed"
+      ? "success"
+      : task.status === "In_Process"
+      ? "primary"
+      : "warning";
+
+  const today = new Date();
+  const dueDate = new Date(task.dueDate);
+
+  // Calculate difference in days (milliseconds / milliseconds_per_day)
+  const msInDay = 1000 * 60 * 60 * 24;
+  const daysLeft = Math.ceil((dueDate.getTime() - today.getTime()) / msInDay);
+  let dueDateText = "";
+  let dueDateClass = "bg-secondary";
+  if (daysLeft < 0) {
+    dueDateText = "Overdue";
+    dueDateClass = "bg-danger"; // Visually mark as overdue
+  } else if (daysLeft === 0) {
+    dueDateText = "Due Today";
+    dueDateClass = "bg-warning"; // Visually mark as due today
+  } else {
+    dueDateText = `Due in ${daysLeft}d`;
+    dueDateClass = "bg-info";
+  }
+  return `
+          <div class="col">
+                            <div class="card border-2  border-${statusColor} rounded-4 h-100">
+                                <div class="card-body ">
+                                    <div class="d-flex">
+                                        <div class="col-10 d-flex flex-column justify-content-between">
+                                            <div class="d-flex align-items-start my-4">
+                                                <input class="form-check-input" type="checkbox" value="${task.id}"
+                                                    id="${task.id}">
+                                                <div class="ps-3 pe-1">
+                                                    <strong class="card-title">${task.title}</strong>
+                                                    <p class="fw-6 text-muted w-75">${task.description}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <small class="bg-danger bg-gradient text-primary-emphasis rounded px-3 py-1">High</small>
+                                                <small class="bg-${statusColor} bg-opacity-75 text-white rounded px-3 py-1 me-2">${statusText}</small>
+                                                    <small class="${dueDateClass} bg-gradient text-primary-emphasis my-2 rounded px-3 py-1">${dueDateText}</small>
+                                            </div>
+                                        </div>
+                                        <div
+                                            class="col-2 d-flex flex-column justify-content-between  my-2 align-items-center" id="task-button">
+                                            <button type="button" class="btn delete" id="${task.id}"><i class="bi bi-trash"></i></button>
+                                           <button type="button" class="btn edit" id="${task.id}"> <i class="bi bi-pen"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+          `;
+}
+
+function updateCountDashboard(
+  totalTask,
+  pendingTask,
+  inProcessTask,
+  completedTask
+) {
+  document.getElementById("totalTask").innerText = totalTask;
+  document.getElementById("pendingTask").innerText = pendingTask;
+  document.getElementById("inProcessTask").innerText = inProcessTask;
+  document.getElementById("completedTask").innerText = completedTask;
+}
+
+document.querySelector(".all-btn-group").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  const currentActive = document.querySelector(".all-btn-group .active");
+  if (currentActive) {
+    currentActive.classList.remove("active");
+  }
+  e.target.classList.add("active");
+
+  const filterTask = e.target.textContent.trim();
+
+  switch (filterTask) {
+    case "Pending":
+      filteredTask = allTasks.filter((task) => task.status === "Pending");
+      break;
+    case "In-Process":
+      filteredTask = allTasks.filter((task) => task.status === "In_Process");
+      break;
+    case "Completed":
+      filteredTask = allTasks.filter((task) => task.status === "Completed");
+      break;
+
+    default:
+      filteredTask = allTasks;
+      break;
+  }
+
+  renderTasks(filteredTask);
+});
+
+async function getAllTasks() {
+  try {
+    const response = await api.get("/all");
+    allTasks = response.data.data || [];
+    filteredTask = allTasks;
+    const tasks = response.data.data;
+    let totalTask = tasks.length || 0,
+      pendingTask = 0,
+      inProcessTask = 0,
+      completedTask = 0;
+    // renderTasks(tasks);
+    tasks.forEach((task) => {
+      if (task.status === "Pending") {
+        pendingTask++;
+      } else if (task.status === "In_Process") {
+        inProcessTask++;
+      } else if (task.status === "Completed") {
+        completedTask++;
+      }
+    });
+    renderTasks(filteredTask);
+
+    updateCountDashboard(totalTask, pendingTask, inProcessTask, completedTask);
+  } catch (error) {
+    sendToastNotification(error.response, "red");
+  }
+}
+
+function renderTasks(tasks) {
+  if (taskContainer) {
+    taskContainer.innerHTML = "";
+    if (tasks.length <= 0) {
+      document.querySelector(
+        "#task-container"
+      ).innerHTML = `<div class="col-12 text-center  py-5">
+                    <p class="text-muted fs-5">No tasks found. Click the button to add a new task! </p>
+                </div>`;
+    } else {
+      tasks.forEach((task) => {
+        const taskHtml = displayTask(task);
+        taskContainer.insertAdjacentHTML("beforeend", taskHtml);
+      });
+    }
+  }
+}
+
+if (taskContainer) {
+  taskContainer.addEventListener("click", (e) => {
+    debugger;
+    const clickedActionBtn = e.target.closest("button");
+    if (!clickedActionBtn) return;
+    const taskId = Number(clickedActionBtn.id);
+    console.log(typeof taskId);
+    if (!taskId) return;
+
+    if (clickedActionBtn.classList.contains("delete")) {
+      deleteTask(taskId);
+    } else {
+      editTask(taskId);
+    }
+  });
+}
+
+async function deleteTask(taskId) {
+  const confirmDelete = confirm("Are you sure want to delete task");
+  if (!confirmDelete) {
+    return;
+  }
+  try {
+    const response = await api.delete(`/${taskId}`);
+    console.log(response);
+    sendToastNotification(response, "green");
+    getAllTasks();
+  } catch (error) {
+    sendToastNotification(error.response, "red");
+  }
+}
+
+async function editTask(taskId) {
+  openAddTaskModal();
+  try {
+    const response = await api.get(`/all`);
+    const task = response.data.data.find((t) => t.id === Number(taskId));
+    titleInput.value = task.title;
+    descriptionInput.value = task.description;
+    dueDateInput.value = new Date(task.dueDate).toISOString().split("T")[0];
+    taskForm.elements["status"].value = task.status;
+    taskForm.dataset.editId = taskId;
+  } catch (error) {
+    sendToastNotification(error.response, "red");
+  }
+}
+
+logoutBtn.addEventListener("click", () => {
+  const confirmation = confirm("are you sure for logout");
+  if (confirmation) {
+    localStorage.clear();
+    location.replace("index.html");
+  }
+});
+
+async function submitTask(e) {
+  if (!taskForm.checkValidity()) {
+    taskForm.classList.add("was-validated");
+    const invalidField = taskForm.querySelectorAll(".is-invalid");
+    invalidField.forEach((input) => {
+      input.classList.add("is-invalid");
+    });
+
+    return;
+  }
+
+  if (dueDateInput.isCustomInvalid) {
+    taskForm.classList.add("was-validated");
+    return;
+  }
+
+  const rawDate = dueDateInput.value;
+  const isoDate = new Date(rawDate).toISOString();
+  const taskData = {
+    title: titleInput.value,
+    description: descriptionInput.value,
+    status: taskForm.elements["status"].value,
+    dueDate: isoDate,
+  };
+  // debugger;
+  const editId = taskForm.dataset.editId;
+  try {
+    let response;
+
+    if (editId) {
+      response = await api.put(`/${editId}`, taskData);
+    } else {
+      response = await api.post("/create", taskData);
+    }
+    sendToastNotification(response, "green");
+    if (response.data.success === true) {
+      closeAddTaskModal();
+      delete taskForm.dataset.editId;
+      taskForm.reset();
+      taskForm.classList.remove("was-validated");
+      getAllTasks();
+    }
+  } catch (error) {
+    console.log(error);
+    sendToastNotification(error.response, "red");
+  }
+}
+submitTaskBtn.addEventListener("click", (e) => {
+  submitTask(e);
+});
+
+export function sendToastNotification(response, clr) {
+  Toastify({
+    text: response?.data?.message || "something went wrong",
+    duration: 3000,
+    style: {
+      background: clr,
+      "z-index": 100,
+      right: 0,
+      top: 20,
+      position: "absolute",
+      maxWidth: "400px",
+      borderRadius: "8px",
+    },
+  }).showToast();
+}
+
+function decodeJwt(token) {
+  try {
+    const base64Url = token.split(".")[1]; // Get the payload part
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/"); // Convert Base64Url to Base64
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  } catch (e) {
+    console.error("Error decoding JWT:", e);
+    return null;
+  }
+}
